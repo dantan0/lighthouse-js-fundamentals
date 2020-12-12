f6235d847bebacddca327eee6f330cd529aaf444
dantan123 <dtan9988@gmail.com>
2020-12-12 23:58:01 +0000

    play around with ice cream list

---
+++ ice-cream.js
@@ -0,0 +1,16 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// step 1
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// step 2
+console.log(iceCreamFlavours[0]);
+
+// step 3
+console.log(iceCreamFlavours[5]);
+
+// step 4
+console.log(iceCreamFlavours.length);
